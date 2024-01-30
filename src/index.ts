54ee280a64ece26df882da14b3294579d873998d
Jack Le Hamster <jacklehamster@gmail.com>
2024-01-30 11:22:40 -0800

    SpriteSheet

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import { MediaData } from "./texture/MediaData";
 import { IImageManager } from "./texture/IImageManager";
 import { TextureManager, TextureId, TEXTURE_INDEX_FOR_VIDEO } from "./texture/TextureManager"
 import { ITextureManager } from "./texture/ITextureManager";
+import { SpriteSheet } from "./texture/spritesheet/SpriteSheet";
 
-export { ImageManager, Media, MediaId, MediaData, IImageManager, TextureManager, TextureId, TEXTURE_INDEX_FOR_VIDEO, ITextureManager }
+export { ImageManager, Media, MediaId, MediaData, IImageManager, TextureManager, TextureId, TEXTURE_INDEX_FOR_VIDEO, ITextureManager, SpriteSheet }
